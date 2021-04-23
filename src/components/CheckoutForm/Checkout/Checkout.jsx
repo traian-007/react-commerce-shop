4e4f55e46398221a83533311a3855d8ad0b53a0e
traian-007 <72014521+traian-007@users.noreply.github.com>
2021-04-23 11:25:42 +0300

    final com

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,25 +1,76 @@
-import { useState } from 'react';
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline } from '@material-ui/core';
+import { commerce } from '../../../lib/commerce'
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import { Link, useHistory } from 'react-router-dom';
 
 const steps = ['Shipping address', 'Payment details']
 
-const Checkout = () => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
+    const [checkoutToken, setCheckoutToken] = useState(null)
     const [activeStep, setActiveStep] = useState(0);
+    const [shippingData, setShippingData] = useState({});
+    // const [isFinished, setIsFinished] = useState(false)
     const classes = useStyles();
+    const history = useHistory();
 
-    const Confirmation = () => (
-        <div>
-            Confirmation
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
+                setCheckoutToken(token)
+            } catch (error) {
+                if (activeStep !== steps.length) history.push('/')
+            }
+        }
+        generateToken();
+    }, [cart]);
+
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
+
+    const next = (data) => {
+        setShippingData(data)
+        nextStep()
+    }
+
+    // const timeout = () => {
+    //     setTimeout(() => {
+    //         setIsFinished(true)
+    //     }, 3000)
+    // }
+
+    let Confirmation = () => (order.customer ? (
+        <>
+            <div>
+                <Typography variant='h5'>Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}!</Typography>
+                <Divider className={classes.divider} />
+                <Typography variant='subtitle2'>Order ref: {order.customer_reference}</Typography>
+            </div>
+            <br />
+            <Button component={Link} to="/" variant='outlined'>Back to Home</Button>
+        </>
+    ) : (
+        <div className={classes.spinner}>
+            <CircularProgress />
         </div>
-    )
+    ))
+
+    if (error) {
+        <>
+            <Typography variant='h5'>Error: {error}</Typography>
+            <br />
+            <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
+        </>
+    }
     const Form = () => activeStep === 0
-        ? <AddressForm />
-        : <PaymentForm />
+        ? <AddressForm checkoutToken={checkoutToken} next={next} />
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout} />
     return (
         <>
+            <CssBaseline />
             <div className={classes.toolbar} />
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
@@ -31,7 +82,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
-                    {activeStep === steps.length ? <Confirmation /> : <Form />}
+                    {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
                 </Paper>
             </main>
         </>
